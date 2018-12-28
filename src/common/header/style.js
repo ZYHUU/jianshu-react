4e84f9fe4eb97e2df42f4cd7abe7938f9cc593cf
zyh <zhangyinghaoweb@163.com>
2018-12-28 19:33:12 +0800

    完成header基本布局

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -20,7 +20,65 @@ export const Logo = styled.a.attrs({
 export const Nav = styled.div`
     width: 960px;
     margin: 0 auto;
+    padding-right: 70px;
+    box-sizing: border-box;
     height: 100%;
-    background: green;
+`;
+export const NavItem = styled.div`
+    line-height: 56px;
+    padding: 0 15px;
+    font-size: 17px;
+    color: #333;
+    &.left {
+        float: left;
+    }
+    &.right {
+        float: right;
+        color: #969696
+    }
+    &.active {
+        color: #ea6f5a;
+    }
+`;
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+})`
+    width: 160px;
+    height: 38px;
+    margin-top: 9px;
+    margin-left: 20px;
+    padding: 0 20px;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+    border-radius: 19px;
+    background: #eee;
+    font-size: 14px;
+    &::placeholder {
+        color: #999;
+    }
+`;
+export const Addition = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 56px;
+`;
+export const Button = styled.div`
+    float: right
+    margin-top: 9px;
+    margin-right: 20px;
+    padding: 0 20px;
+    line-height: 38px;
+    border-radius: 19px;
+    border: 1px solid #ec6149;
+    font-size: 14px;
+    &.reg {
+        color: #ec6149;
+    }
+    &.writting {
+        color: #fff;
+        background: #ec6149;
+    }
 `;
 
