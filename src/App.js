658117f2312408d9e63dfa06fa8cd7402d36ebf7
Zyh <zhangyinghaoweb@163.com>
2018-12-28 17:50:32 +0800

    header 部分

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Header from './common/header/index'
 class App extends Component {
   render() {
     return (   
       <div className="App">
+        <Header/>
         hello world
       </div>   
     );
---
+++ src/common/header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { 
+    HeaderWrapper,
+    Logo,
+    Nav
+ } from './style'
+class Header extends Component {
+    render() {
+       return (
+          <HeaderWrapper>
+            <Logo />
+            <Nav />
+          </HeaderWrapper>  
+       ) 
+    }
+}
+
+export default Header
